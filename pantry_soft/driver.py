7d9cb20d0dc4dbf4fa6f99e0fc88a1865cc414d3
bibhubhatta <82817444+bibhubhatta@users.noreply.github.com>
2024-02-27 02:46:28 -0500

    Refactor

--- pantry_soft/driver.py
+++ pantry_soft/driver.py
@@ -1,26 +1,20 @@
 from time import sleep
 
 from selenium import webdriver
-from selenium.common import TimeoutException
-from selenium.webdriver import Keys
+from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
 
 from inventory.item import Item
 
 
 class PantrySoftDriver:
-    """Web driver for PantrySoft.
-    Stores the Selenium WebDriver and provides methods to interact with the PantrySoft website.
-    """
+    """Web driver for PantrySoft."""
 
     def __init__(self, url: str, username: str, password: str):
-        """Initialize a PantrySoftDriver object.
-        Args:
-            url (str): The URL of the PantrySoft website.
-            username (str): The username to log in with.
-            password (str): The password to log in with.
-        """
+        """Initialize a PantrySoftDriver object."""
         self.driver = self.__get_driver(url, username, password)
 
     def get_php_session(self) -> str:
@@ -30,10 +24,6 @@ class PantrySoftDriver:
     @staticmethod
     def __get_driver(url: str, username: str, password: str) -> webdriver.Chrome:
         """Return a Selenium WebDriver."""
-        # Start driver headless
-        # options = webdriver.ChromeOptions()
-        # options.add_argument("--headless")
-        # driver = webdriver.Chrome(options=options)
         driver = webdriver.Chrome()
         driver.get(url)
 
@@ -41,86 +31,57 @@ class PantrySoftDriver:
         driver.find_element(By.ID, "password").send_keys(password)
         driver.find_element(By.ID, "index_login_btn").click()
 
-        # Wait for the page to load
-        max_wait: int = 10
         try:
-            # find <a href="/inventoryitem/">Items</a>
-            WebDriverWait(driver, max_wait).until(
-                lambda d: d.find_element(By.XPATH, "//a[@href='/inventoryitem/']")
+            WebDriverWait(driver, 10).until(
+                EC.presence_of_element_located(
+                    (By.XPATH, "//a[@href='/inventoryitem/']")
+                )
             )
         except TimeoutException:
-            print(f"Timed out waiting for page to load after {max_wait} seconds.")
+            print("Timed out waiting for page to load.")
             driver.quit()
             raise
 
         return driver
 
     def add_item(self, item: Item):
-        # TODO: driver should not have dependencies on the Item class
         """Add an item to the pantry."""
         self.driver.get("https://app.pantrysoft.com/inventoryitem/new")
         sleep(1)
 
-        name_field = self.driver.find_element(By.ID, "pantrybundle_inventoryitem_name")
-        sleep(0.1)
-        item_number_field = self.driver.find_element(
-            By.ID, "pantrybundle_inventoryitem_itemNumber"
-        )
-        sleep(0.1)
-        weight_field = self.driver.find_element(
-            By.ID, "pantrybundle_inventoryitem_weight"
-        )
-        sleep(0.1)
+        self.__fill_input_field("pantrybundle_inventoryitem_name", item.name)
+        self.__fill_input_field("pantrybundle_inventoryitem_itemNumber", item.upc)
+        self.__fill_input_field("pantrybundle_inventoryitem_weight", str(item.size))
 
-        name_field.clear()
-        name_field.send_keys(item.name)
-        sleep(0.15)
-
-        item_number_field.clear()
-        item_number_field.send_keys(item.upc)
-        sleep(0.15)
-
-        weight_field.clear()
-        weight_field.send_keys(item.size)
-        sleep(0.15)
-
-        # Unit and category are difficult to deal with because they are many to many relationships, so we'll skip them
-        # for now.
-        # Description also seems like a foreign key relationship, so we'll skip that for now.
-
-        submit_button = self.driver.find_element(
-            By.XPATH, '//*[@id="blueBar"]/div/div[3]/button'
-        )
-        submit_button.send_keys(Keys.ENTER)
-        sleep(0.5)
+        self.__submit("//button[@class='btn btn-default' and @type='submit']")
 
     def link_code_to_item(self, item: Item):
         """Links UPC code to an item."""
         self.driver.get("https://app.pantrysoft.com/inventory_code/")
         sleep(1)
 
-        # click on the <button type="button" class="btn btn-default" onclick="newItemCode()">
-        #         New Item Code
-        #     </button>
-
-        # wait until the button is clickable
-        WebDriverWait(self.driver, 10).until(
-            lambda d: d.find_element(By.CLASS_NAME, "btn-default")
+        new_code_button = WebDriverWait(self.driver, 10).until(
+            EC.element_to_be_clickable((By.CLASS_NAME, "btn-default"))
         )
-        self.driver.find_element(By.CLASS_NAME, "btn-default").send_keys(Keys.ENTER)
+        new_code_button.send_keys(Keys.ENTER)
         sleep(0.6)
 
-        # find text field with id pantrybundle_inventoryitemcode_codeNumber
-        code_number_field = self.driver.find_element(
-            By.ID, "pantrybundle_inventoryitemcode_codeNumber"
-        )
-        code_number_field.send_keys(item.upc)
-        sleep(0.15)
+        self.__fill_input_field("pantrybundle_inventoryitemcode_codeNumber", item.upc)
 
-        # <input aria-autocomplete="list" aria-labelledby="vs4__combobox" aria-controls="vs4__listbox" type="search" autocomplete="off" class="vs__search">
         search_field = self.driver.find_element(By.CLASS_NAME, "vs__search")
         search_field.send_keys(item.name)
-
-        # Pause for one second and press enter on the search field
         sleep(1)
         search_field.send_keys(Keys.ENTER)
+
+    def __fill_input_field(self, field_id: str, value: str):
+        """Fill an input field."""
+        field = self.driver.find_element(By.ID, field_id)
+        field.clear()
+        field.send_keys(value)
+        sleep(0.5)
+
+    def __submit(self, button_xpath: str):
+        """Submit the form by clicking a button."""
+        submit_button = self.driver.find_element(By.XPATH, button_xpath)
+        submit_button.send_keys(Keys.ENTER)
+        sleep(0.5)
--- pantry_soft/pantrysoft.py
+++ pantry_soft/pantrysoft.py
@@ -13,125 +13,50 @@ class PantrySoft:
 
     def __init__(self, url: str, username: str, password: str):
         """Initialize a PantrySoft object."""
-
         self.driver = PantrySoftDriver(url, username, password)
         self.php_session = self.driver.get_php_session()
 
-    def get_json(self, endpoint: str, indexdata: str):
-        """Return the JSON response from the PantrySoft API."""
-        cookies = {
-            "PHPSESSID": self.php_session,
+    def _get_request_params(self) -> dict:
+        """Generate common request parameters."""
+        return {
+            "cookies": {"PHPSESSID": self.php_session},
+            "headers": {
+                "authority": "app.pantrysoft.com",
+                "accept": "application/json, text/javascript, */*; q=0.01",
+                "accept-language": "en-US,en;q=0.9",
+                "sec-ch-ua": '"Not_A Brand";v="8", "Chromium";v="120", "Google Chrome";v="120"',
+                "sec-ch-ua-mobile": "?0",
+                "sec-fetch-mode": "cors",
+                "sec-fetch-site": "same-origin",
+                "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
+                "x-requested-with": "XMLHttpRequest",
+            },
         }
 
-        headers = {
-            "authority": "app.pantrysoft.com",
-            "accept": "application/json, text/javascript, */*; q=0.01",
-            "accept-language": "en-US,en;q=0.9",
-            "referer": f"https://app.pantrysoft.com/{endpoint}/",
-            "sec-ch-ua": '"Not_A Brand";v="8", "Chromium";v="120", "Google Chrome";v="120"',
-            "sec-ch-ua-mobile": "?0",
-            "sec-fetch-mode": "cors",
-            "sec-fetch-site": "same-origin",
-            "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
-            "x-requested-with": "XMLHttpRequest",
-        }
-
-        params = {
-            "_": str(int(time.time() * 1000)),
-        }
-
+    def _make_get_request(self, endpoint: str, indexdata: str) -> dict:
+        """Make a GET request to the PantrySoft API."""
+        params = self._get_request_params()
+        params["params"] = {"_": str(int(time.time() * 1000))}
         response = requests.get(
-            f"https://app.pantrysoft.com/{endpoint}/{indexdata}",
-            params=params,
-            cookies=cookies,
-            headers=headers,
+            f"https://app.pantrysoft.com/{endpoint}/{indexdata}", **params
         )
-
         return response.json()
 
     def get_all_items_json(self) -> dict:
-        """Return the JSON response from the PantrySoft API.
-
-        Sample response:
-        {
-            "data": [
-                {
-                    "id": 1,
-                    "itemNumber": "1",
-                    "name": "French Cut Green Beans 14.25oz",
-                    "unit": "Can",
-                    "value": "0.00",
-                    "weight": "0.00",
-                    "creditPrice": null,
-                    "isActive": true,
-                    "sortOrder": 0,
-                    "itemTypeString": "Canned Vegetables",
-                    "inventoryItemTags": null
-                },
-                {
-                    "id": 798,
-                    "itemNumber": "044000882105",
-                    "name": "RITZ Peanut Butter Sandwich Crackers, 8 - 1.38 oz Snack Packs",
-                    "unit": "Ounces",
-                    "value": "0.00",
-                    "weight": "11.04",
-                    "creditPrice": "0.00",
-                    "isActive": true,
-                    "sortOrder": 780,
-                    "itemTypeString": "Food",
-                    "inventoryItemTags": null
-                }
-            ]
-        }
-
-
-        """
-        return self.get_json("inventoryitem", "indexdata")
+        """Return the JSON response for all items."""
+        return self._make_get_request("inventoryitem", "indexdata")
 
     def get_all_inventory_codes_json(self) -> dict:
-        """Return the JSON response from the PantrySoft API.
-
-        Sample response:
-        {
-            "message": "success",
-            "data": [
-                {
-                    "itemName": "Bananas",
-                    "itemId": 2,
-                    "codeId": 6,
-                    "codeNumber": "012",
-                    "codeDescription": null,
-                    "itemNumber": "2"
-                },
-                {
-                    "itemName": "Mixed Nuts",
-                    "itemId": 784,
-                    "codeId": 1,
-                    "codeNumber": "076811000140",
-                    "codeDescription": null,
-                    "itemNumber": "076811000140"
-                },
-                {
-                    "itemName": "RITZ Peanut Butter Sandwich Crackers, 8 - 1.38 oz Snack Packs",
-                    "itemId": 798,
-                    "codeId": 7,
-                    "codeNumber": "044000882105",
-                    "codeDescription": null,
-                    "itemNumber": "044000882105"
-                }
-            ]
-        }
-
-        """
-        return self.get_json("inventory_code", "indexData")
+        """Return the JSON response for all inventory codes."""
+        return self._make_get_request("inventory_code", "indexData")
 
     def get_all_item_types_json(self) -> dict:
-        """Return the JSON response from the PantrySoft API."""
-        return self.get_json("inventoryitemtype", "indexdata")
+        """Return the JSON response for all item types."""
+        return self._make_get_request("inventoryitemtype", "indexdata")
 
     def get_all_item_tags_json(self) -> dict:
-        """Return the JSON response from the PantrySoft API."""
-        return self.get_json("inventoryitemtag", "indexdata")
+        """Return the JSON response for all item tags."""
+        return self._make_get_request("inventoryitemtag", "indexdata")
 
     def add_item(self, item: Item) -> None:
         """Add an item to the PantrySoft inventory."""
@@ -141,48 +66,13 @@ class PantrySoft:
 
     def delete_item(self, item_id: int) -> None:
         """Delete an item from the PantrySoft inventory."""
-
-        cookies = {
-            "PHPSESSID": self.php_session,
-        }
-
-        headers = {
-            "authority": "app.pantrysoft.com",
-            "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
-            "accept-language": "en-US,en;q=0.9",
-            "cache-control": "max-age=0",
-            "content-type": "application/x-www-form-urlencoded",
-            "origin": "https://app.pantrysoft.com",
-            "referer": "https://app.pantrysoft.com/inventoryitem/",
-            "sec-ch-ua": '"Not_A Brand";v="8", "Chromium";v="120", "Google Chrome";v="120"',
-            "sec-ch-ua-mobile": "?0",
-            "sec-ch-ua-platform": '"Windows"',
-            "sec-fetch-dest": "document",
-            "sec-fetch-mode": "navigate",
-            "sec-fetch-site": "same-origin",
-            "sec-fetch-user": "?1",
-            "upgrade-insecure-requests": "1",
-            "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
-        }
-
-        response = requests.get(
-            "https://app.pantrysoft.com/inventoryitem/",
-            cookies=cookies,
-            headers=headers,
-        )
-
-        response_html = response.text
-        soup = BeautifulSoup(response_html, "html.parser")
+        params = self._get_request_params()
+        response = requests.get("https://app.pantrysoft.com/inventoryitem/", **params)
+        soup = BeautifulSoup(response.text, "html.parser")
         csrf_token = soup.find("generic-delete-modal").get("csrf-token")
-
-        data = {
-            "_method": "DELETE",
-            "csrfToken": csrf_token,
-        }
-
+        data = {"_method": "DELETE", "csrfToken": csrf_token}
         requests.post(
             f"https://app.pantrysoft.com/inventoryitem/delete/{item_id}",
-            cookies=cookies,
-            headers=headers,
+            **params,
             data=data,
         )
