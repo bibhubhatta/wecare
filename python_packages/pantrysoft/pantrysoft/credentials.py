00f5057d41c378f126e4ecd62cc5c1bc9671c93a
bibhubhatta <82817444+bibhubhatta@users.noreply.github.com>
2024-11-12 17:14:52 -0500

    Use .env

--- python_packages/pantrysoft/pantrysoft/credentials.py
+++ python_packages/pantrysoft/pantrysoft/credentials.py
@@ -1,5 +1,15 @@
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 PANTRYSOFT_URL = os.environ.get("PANTRYSOFT_URL")
 PANTRYSOFT_USERNAME = os.environ.get("PANTRYSOFT_USERNAME")
 PANTRYSOFT_PASSWORD = os.environ.get("PANTRYSOFT_PASSWORD")
+
+if not PANTRYSOFT_URL:
+    raise ValueError("PANTRYSOFT_URL environment variable not set")
+if not PANTRYSOFT_USERNAME:
+    raise ValueError("PANTRYSOFT_USERNAME environment variable not set")
+if not PANTRYSOFT_PASSWORD:
+    raise ValueError("PANTRYSOFT_PASSWORD environment variable not set")
