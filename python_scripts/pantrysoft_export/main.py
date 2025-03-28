c062c390ad3ad76b4cce2f2635f26a07f5ca2407
bibhubhatta <82817444+bibhubhatta@users.noreply.github.com>
2025-03-28 13:55:35 -0400

    Add session ID retrieval

--- python_scripts/pantrysoft_export/main.py
+++ python_scripts/pantrysoft_export/main.py
@@ -1,5 +1,25 @@
+import os
+
+from dotenv import load_dotenv
+
+from pantrysoft_authenticator import get_php_session_id
+
+
 def main():
-    print("Hello from pantrysoft-export!")
+    load_dotenv()
+    username = get_from_env("PANTRYSOFT_USERNAME")
+    password = get_from_env("PANTRYSOFT_PASSWORD")
+
+    session_id = get_php_session_id(username, password)
+    print("session_id:", session_id)
+
+
+def get_from_env(key: str) -> str:
+    """Get an environment variable."""
+    value = os.getenv(key)
+    if value is None:
+        raise ValueError(f"Environment variable {key} not found.")
+    return value
 
 
 if __name__ == "__main__":
