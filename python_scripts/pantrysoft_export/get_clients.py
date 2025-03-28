942007c1b8c46662ce39237732fb5950e86e5b76
bibhubhatta <82817444+bibhubhatta@users.noreply.github.com>
2025-03-28 14:25:58 -0400

    Add client retrieval

---
+++ python_scripts/pantrysoft_export/get_clients.py
@@ -0,0 +1,32 @@
+import requests
+
+def get_clients(php_session_id: str) -> dict[str, str]:
+
+    url = "https://app.pantrysoft.com/client/client_index_data?specFilter=client"
+
+    payload = "draw=2&columns%5B0%5D%5Bdata%5D=clientId&columns%5B0%5D%5Bname%5D=clientId&columns%5B0%5D%5Bsearchable%5D=true&columns%5B0%5D%5Borderable%5D=true&columns%5B0%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B0%5D%5Bsearch%5D%5Bregex%5D=false&columns%5B1%5D%5Bdata%5D=locationName&columns%5B1%5D%5Bname%5D=locationName&columns%5B1%5D%5Bsearchable%5D=true&columns%5B1%5D%5Borderable%5D=true&columns%5B1%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B1%5D%5Bsearch%5D%5Bregex%5D=false&columns%5B2%5D%5Bdata%5D=deliveryRoute&columns%5B2%5D%5Bname%5D=deliveryRoute&columns%5B2%5D%5Bsearchable%5D=true&columns%5B2%5D%5Borderable%5D=true&columns%5B2%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B2%5D%5Bsearch%5D%5Bregex%5D=false&columns%5B3%5D%5Bdata%5D=locationCounty&columns%5B3%5D%5Bname%5D=locationCounty&columns%5B3%5D%5Bsearchable%5D=true&columns%5B3%5D%5Borderable%5D=true&columns%5B3%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B3%5D%5Bsearch%5D%5Bregex%5D=false&columns%5B4%5D%5Bdata%5D=accountNumber&columns%5B4%5D%5Bname%5D=accountNumber&columns%5B4%5D%5Bsearchable%5D=true&columns%5B4%5D%5Borderable%5D=true&columns%5B4%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B4%5D%5Bsearch%5D%5Bregex%5D=false&columns%5B5%5D%5Bdata%5D=businessName&columns%5B5%5D%5Bname%5D=businessName&columns%5B5%5D%5Bsearchable%5D=true&columns%5B5%5D%5Borderable%5D=true&columns%5B5%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B5%5D%5Bsearch%5D%5Bregex%5D=false&columns%5B6%5D%5Bdata%5D=firstName&columns%5B6%5D%5Bname%5D=firstName&columns%5B6%5D%5Bsearchable%5D=true&columns%5B6%5D%5Borderable%5D=true&columns%5B6%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B6%5D%5Bsearch%5D%5Bregex%5D=false&columns%5B7%5D%5Bdata%5D=lastName&columns%5B7%5D%5Bname%5D=lastName&columns%5B7%5D%5Bsearchable%5D=true&columns%5B7%5D%5Borderable%5D=true&columns%5B7%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B7%5D%5Bsearch%5D%5Bregex%5D=false&columns%5B8%5D%5Bdata%5D=birthday&columns%5B8%5D%5Bname%5D=birthday&columns%5B8%5D%5Bsearchable%5D=true&columns%5B8%5D%5Borderable%5D=true&columns%5B8%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B8%5D%5Bsearch%5D%5Bregex%5D=false&columns%5B9%5D%5Bdata%5D=status&columns%5B9%5D%5Bname%5D=status&columns%5B9%5D%5Bsearchable%5D=true&columns%5B9%5D%5Borderable%5D=true&columns%5B9%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B9%5D%5Bsearch%5D%5Bregex%5D=false&columns%5B10%5D%5Bdata%5D=streetAddress&columns%5B10%5D%5Bname%5D=streetAddress&columns%5B10%5D%5Bsearchable%5D=true&columns%5B10%5D%5Borderable%5D=true&columns%5B10%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B10%5D%5Bsearch%5D%5Bregex%5D=false&columns%5B11%5D%5Bdata%5D=unitNo&columns%5B11%5D%5Bname%5D=unitNo&columns%5B11%5D%5Bsearchable%5D=true&columns%5B11%5D%5Borderable%5D=true&columns%5B11%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B11%5D%5Bsearch%5D%5Bregex%5D=false&columns%5B12%5D%5Bdata%5D=phoneNumber&columns%5B12%5D%5Bname%5D=phoneNumber&columns%5B12%5D%5Bsearchable%5D=true&columns%5B12%5D%5Borderable%5D=true&columns%5B12%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B12%5D%5Bsearch%5D%5Bregex%5D=false&order%5B0%5D%5Bcolumn%5D=7&order%5B0%5D%5Bdir%5D=asc&start=0&length=100&search%5Bvalue%5D=&search%5Bregex%5D=false"
+    headers = {
+        "accept": "application/json, text/javascript, */*; q=0.01",
+        "accept-language": "en-US,en;q=0.9",
+        "content-type": "application/x-www-form-urlencoded; charset=UTF-8",
+        "cookie": f"PHPSESSID={php_session_id}",
+        "origin": "https://app.pantrysoft.com",
+        "priority": "u=1, i",
+        "referer": "https://app.pantrysoft.com/client/",
+        "sec-ch-ua": '"Not A(Brand";v="8", "Chromium";v="132", "Google Chrome";v="132"',
+        "sec-ch-ua-mobile": "?0",
+        "sec-ch-ua-platform": '"macOS"',
+        "sec-fetch-dest": "empty",
+        "sec-fetch-mode": "cors",
+        "sec-fetch-site": "same-origin",
+        "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/132.0.0.0 Safari/537.36",
+        "x-requested-with": "XMLHttpRequest",
+    }
+
+    response = requests.request("POST",
+                                url,
+                                headers=headers,
+                                data=payload
+                                )
+
+    return response.json()
--- python_scripts/pantrysoft_export/main.py
+++ python_scripts/pantrysoft_export/main.py
@@ -1,18 +1,37 @@
 import os
 
 from dotenv import load_dotenv
-
+from get_clients import get_clients
 from pantrysoft_authenticator import get_php_session_id
 
 
 def main():
+    session_id = get_session_id()
+
+    clients = get_clients(session_id)
+    print(clients)
+    
+
+def get_session_id() -> str:
+    """Get the PHP session ID."""
+
+    # Check if cached session ID is available
+    SESSION_FILE = "session_id.txt"
+    if os.path.exists(SESSION_FILE):
+        with open(SESSION_FILE, "r") as file:
+            return file.read().strip()
+
     load_dotenv()
     username = get_from_env("PANTRYSOFT_USERNAME")
     password = get_from_env("PANTRYSOFT_PASSWORD")
 
     session_id = get_php_session_id(username, password)
-    print("session_id:", session_id)
+    # Save the session ID to a file
+    with open(SESSION_FILE, "w") as file:
+        file.write(session_id)
 
+    return session_id
+    
 
 def get_from_env(key: str) -> str:
     """Get an environment variable."""
