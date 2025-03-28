2dd9a0d6636c9565e32da51b6e2bf39d87d3323a
bibhubhatta <82817444+bibhubhatta@users.noreply.github.com>
2025-03-28 17:57:48 -0400

    Update import after refactor

--- python_packages/pantrysoft_authenticator/pantrysoft_authenticator/autheticate.py
+++ python_packages/pantrysoft_authenticator/pantrysoft_authenticator/autheticate.py
@@ -1,11 +1,11 @@
-from pantrysoft_authenticator.playwright_driver import PantrySoftDriver
+from pantrysoft_authenticator.playwright_driver import PlaywrightPantrySoftDriver
 
 PANTRYSOFT_URL = "https://app.pantrysoft.com"
 
 
 def get_php_session_id(username: str, password: str) -> str:
     """Authenticate with PantrySoft."""
-    pantrysoft_driver = PantrySoftDriver(PANTRYSOFT_URL, username, password)
+    pantrysoft_driver = PlaywrightPantrySoftDriver(PANTRYSOFT_URL, username, password)
     try:
         return pantrysoft_driver.get_php_session()
     except Exception as e:
