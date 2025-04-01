35ebbaed63b6671e44074dc0f2a4309f04458d81
bibhubhatta <82817444+bibhubhatta@users.noreply.github.com>
2025-04-01 18:17:28 -0400

    Refactor database handling and add CSV export
    Refactor the database handling by creating global repository 
    instances for clients and client dashboards. Introduce a new 
    function to export client visit dates and client data to CSV 
    files. Update the main execution flow to call the new 
    preparation and export functions, improving code organization 
    and functionality.

--- python_scripts/pantrysoft_export/main.py
+++ python_scripts/pantrysoft_export/main.py
@@ -10,26 +10,47 @@ from joblib import Memory
 from pantrysoft_authenticator import get_php_session_id
 from wrappers import ClientDashboard, ClientDashboardRepositorySql, ClientRepositorySql
 
+SQLITE_ENGINE = sqlalchemy.create_engine("sqlite:///export/pantrysoft.db")
+CLIENT_REPO = ClientRepositorySql(SQLITE_ENGINE)
+CLIENT_DASHBOARD_REPO = ClientDashboardRepositorySql(SQLITE_ENGINE)
 
-def main():
+
+def export_to_csv():
+    dashboards = CLIENT_DASHBOARD_REPO.get_all()
+    client_vist_dates = []
+    for dashboard in dashboards:
+        visit_dates = dashboard.client_visit_dates
+        for visit_date in visit_dates:
+            client_vist_dates.append(
+                {
+                    "client_id": dashboard.client_id,
+                    "visit_date": visit_date,
+                }
+            )
+
+    save_to_csv(client_vist_dates, "export/client_vist_dates.csv")
+
+    clients = CLIENT_REPO.get_all()
+    client_data = [client.client_dict for client in clients]
+    save_to_csv(client_data, "export/clients.csv")
+
+
+def prepare_database():
     session_id = get_session_id()
-    sqlite_engine = sqlalchemy.create_engine("sqlite:///export/pantrysoft.db")
-    client_repo = ClientRepositorySql(sqlite_engine)
-    client_dashboard_repo = ClientDashboardRepositorySql(sqlite_engine)
 
     # Get clients from Pantrysoft
     clients = get_client_list(session_id)
 
     for client in clients:
-        if client in client_repo and client_repo.get(client.id) is not None:
+        if client in CLIENT_REPO and CLIENT_REPO.get(client.id) is not None:
             print(f"Client {client.id} already exists in the database.")
         else:
             print(f"Client {client.id} does not exist in the database. Saving...")
-            client_repo.save(client)
+            CLIENT_REPO.save(client)
 
         if (
-            client.id in client_dashboard_repo
-            and client_dashboard_repo.get(client.id) is not None
+            client.id in CLIENT_DASHBOARD_REPO
+            and CLIENT_DASHBOARD_REPO.get(client.id) is not None
         ):
             print(f"Client dashboard {client.id} already exists in the database.")
         else:
@@ -40,7 +61,7 @@ def main():
                 client.id, session_id
             )
             client_dashboard = ClientDashboard(client_dashboard_html)
-            client_dashboard_repo.save(client_dashboard)
+            CLIENT_DASHBOARD_REPO.save(client_dashboard)
 
     print("All clients and dashboards have been saved to the database.")
 
@@ -62,7 +83,6 @@ def save_html_file(html: str, file_path: str) -> None:
         print(f"An error occurred while writing HTML to file: {e}")
 
 
-@Memory(".cache", verbose=0).cache
 def save_to_csv(data: list[dict[str, Any]], csv_file_path: str):
     """Saves a list of dictionaries to a CSV file.
 
@@ -154,4 +174,5 @@ def get_from_env(key: str) -> str:
 
 
 if __name__ == "__main__":
-    main()
+    prepare_database()
+    export_to_csv()
